22605df4665bcfe617402139219a1dbeb9290159
ThanhHung1011-96 <65081828+ThanhHung1011-96@users.noreply.github.com>
2025-02-06 23:44:07 +0700

    init commit

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import Button from '@mui/material/Button'
+
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
@@ -14,6 +19,14 @@ const App = () => {
           Edit <code>src/App.js</code> and save to reload.
           Test git
         </p>
+        <FormGroup>
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+          <FormControlLabel required control={<Checkbox />} label="Required" />
+          <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+        </FormGroup>
+        <Button variant="contained">
+          Button
+        </Button>
         <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
