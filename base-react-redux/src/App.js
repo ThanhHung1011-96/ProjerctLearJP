cbd24dfb5ca601e795941f0acc4032aaaeb12c33
ThanhHung1011-96 <65081828+ThanhHung1011-96@users.noreply.github.com>
2025-02-06 22:31:15 +0700

    Test git

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Test git
         </p>
         <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
